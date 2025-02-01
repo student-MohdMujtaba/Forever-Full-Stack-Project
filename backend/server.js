7f2ddf246c80b100313b141f7d5e58793ee71ad0
Mohd Mujtaba <158465099+student-MohdMujtaba@users.noreply.github.com>
2025-02-01 09:40:46 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,8 @@ app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
 app.use('/api/order', orderRouter)
 
-app.get('/',(req,res)=>{
-    res.send("API Working");
-})
+// app.get('/',(req,res)=>{
+//     res.send("API Working");
+// })
 
 app.listen(port, ()=> console.log("Server started on PORT : " + port))
